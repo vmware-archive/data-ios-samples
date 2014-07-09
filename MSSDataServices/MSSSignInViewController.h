008b1e650914ad93ec65cc633efc9d1b71317319
Adrian Kemp <adrian.kemp@me.com>
2014-07-09 09:12:50 -0400

    Initial Commit

---
+++ MSSDataServices/MSSSignInViewController.h
@@ -0,0 +1,13 @@
+//
+//  MSSSignInViewController.h
+//  MSSDataServices Example
+//
+//  Created by Elliott Garcea on 2014-06-06.
+//  Copyright (c) 2014 Pivotal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSSSignInViewController : UIViewController
+
+@end
