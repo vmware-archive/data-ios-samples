8b533b97eb65c0c8f512761888d0d666ec2cadc2
Devin Fallak and Joshua Winters <pair+df+jwinters@pivotal.io>
2014-12-05 15:53:46 -0500

    @pivotal-devin-fallak: Added a text view to show errors

--- PCFData.framework/Headers/PCFRequestCache.h
+++ PCFData.framework/Headers/PCFRequestCache.h
@@ -7,8 +7,17 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@class PCFPendingRequest, PCFOfflineStore;
 
 @interface PCFRequestCache : NSObject
+
+- (instancetype)initWithDefaults:(NSUserDefaults *)defaults;
+
++ (PCFRequestCache *)sharedInstance;
+
++ (PCFRequestCache *)sharedInstanceWithDefaults:(NSUserDefaults *)defaults;
 
 - (void)queueGetWithToken:(NSString *)accessToken collection:(NSString *)collection key:(NSString *)key;
 
@@ -16,4 +25,23 @@
 
 - (void)queueDeleteWithToken:(NSString *)accessToken collection:(NSString *)collection key:(NSString *)key fallback:(NSString *)fallback;
 
+- (void)queuePendingRequest:(PCFPendingRequest *)request;
+
+- (void)retrieveAndExecutePendingRequestsWithToken:(NSString *)accessToken completionHandler:(void (^)(UIBackgroundFetchResult))completionHandler;
+
+- (void)executePendingRequestsWithToken:(NSString *)accessToken requests:(NSMutableArray *)requests;
+
+- (PCFOfflineStore *)createOfflineStoreWithCollection:(NSString *)collection;
+
 @end
+
+
+@interface PCFPendingRequest : NSObject
+
+@property NSDictionary *values;
+
+- (instancetype)initWithDictionary:(NSDictionary *)values;
+
+- (instancetype)initWithMethod:(int)method accessToken:(NSString *)accessToken collection:(NSString *)collection key:(NSString *)key value:(NSString *)value fallback:(NSString *)fallback;
+
+@end 
