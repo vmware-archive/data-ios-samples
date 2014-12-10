18aae5be8883b539f112cf42ca5ea711a02a6cbd
Devin Fallak and Joshua Winters <jwinters@pivotal.io>
2014-12-10 17:31:57 -0500

    Moved PCFData.framework to top level and hooked into new logging method

--- PCFDataSample/AppDelegate.m
+++ PCFDataSample/AppDelegate.m
@@ -15,9 +15,15 @@
 
 @implementation AppDelegate
 
++ (void)load {
+    //[PCFLogger setDebug:true];
+}
+
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    [[PCFRequestCache sharedInstance] executePendingRequestsWithToken:kAccessToken];
+    
     return YES;
 }
 
@@ -44,8 +50,7 @@
 }
 
 - (void)application:(UIApplication *)application performFetchWithCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
-    NSString *token = @"eyJhbGciOiJSUzI1NiJ9.eyJqdGkiOiIwZjVmNThhNy0zNjFmLTQ5Y2ItYjQ4ZS1mMmJhMDAzOTk5MzMiLCJzdWIiOiJkOTI4YTAwYi1lYmZhLTQ3MGItOTc5MC0xYWVhYjI1YjI0ZmQiLCJzY29wZSI6WyJwYXNzd29yZC53cml0ZSIsIm9wZW5pZCIsImNsb3VkX2NvbnRyb2xsZXIud3JpdGUiLCJjbG91ZF9jb250cm9sbGVyLnJlYWQiXSwiY2xpZW50X2lkIjoiY2YiLCJjaWQiOiJjZiIsInVzZXJfaWQiOiJkOTI4YTAwYi1lYmZhLTQ3MGItOTc5MC0xYWVhYjI1YjI0ZmQiLCJ1c2VyX25hbWUiOiJqd2ludGVyc0BwaXZvdGFsLmlvIiwiZW1haWwiOiJqd2ludGVyc0BwaXZvdGFsLmlvIiwiaWF0IjoxNDE3NTQ5NTUzLCJleHAiOjE0MTc1OTI3NTMsImlzcyI6Imh0dHBzOi8vdWFhLmtvbmEuY29mZmVlLmNmbXMtYXBwcy5jb20vb2F1dGgvdG9rZW4iLCJhdWQiOlsib3BlbmlkIiwiY2xvdWRfY29udHJvbGxlciIsInBhc3N3b3JkIl19.rDnLVIH1I1ICa_cph4wWAIsdoqqCsnYJ0Z8SrOYpv_DCOAwZIYCObpJ-AqXa_fXQir6tcTP3ukR79SZNQA188njYY4bMdURPt8rULSIdvxYQurcFrsXZELey74dgUeJbd-iOW2TB1rMaZeYpFtbFAZXVx3NoIftRJxihMBo5DNd6A_KnrjrmBcO0ddbWgjkaCqvvpqEmURb1KNeO1XdBBwlMFzFhEr-5hQ5tEz2O8WGv_lIpAVOByBIWyw_i-kZtFpwpdoADq8D3kijJpAg1NTJ_E3hdHC0UsNcZ74PcLOB686EsWsvJuxvIrk4A14F6pEf_BQ0RxxmlujuMD7JxVQ";
-    [[PCFRequestCache sharedInstance] executePendingRequestsWithToken:token completionHandler:completionHandler];
+    [[PCFRequestCache sharedInstance] executePendingRequestsWithToken:kAccessToken completionHandler:completionHandler];
 }
 
 @end
